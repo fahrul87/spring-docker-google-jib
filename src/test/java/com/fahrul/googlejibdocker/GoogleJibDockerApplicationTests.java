a25480b147e9624680f1be9c3d7d0355a0d6ca51
Fahrul Arya Sugita <faryasugita@gmail.com>
2021-06-30 08:22:43 +0700

    isitimiwir

---
+++ src/test/java/com/fahrul/googlejibdocker/GoogleJibDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahrul.googlejibdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleJibDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
